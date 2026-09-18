15d10a6a48e51a0763478a16c588da271ebe4e3b
montacore <john.t.nallie@gmail.com>
2026-09-17 22:05:25 -0400

    updates to complete functions/write_file.py and test_write_file.py

--- functions/write_file.py
+++ functions/write_file.py
@@ -5,16 +5,16 @@ def write_file(working_directory: str, file_path: str, content: str) -> str:
         abs_path = os.path.abspath(working_directory)
         target_path = os.path.normpath(os.path.join(abs_path, file_path))
         valid_target_path = os.path.commonpath([abs_path, target_path]) == abs_path
+        if not valid_target_path:
+            return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
         if os.path.isdir(target_path):
             return f'Error: Cannot write to "{file_path}" as it is a directory'
-        if not valid_target_path:
-            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
         else:
-            os.makedirs(target_path, exist_ok=True)
+            os.makedirs(os.path.dirname(target_path), exist_ok=True)
             with open(target_path, "w") as f:
                 f.write(content)
                         
-        return f'Successfully wrote to "{file_path} ({len(content)} characters written)'
+        return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
     except Exception as e: 
         return f"Error: {e}"
 
